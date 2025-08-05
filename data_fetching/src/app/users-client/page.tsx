a0373f94d61355cf4d5858fd84aedb11a39a19da
DUDE <nisbis194@gmail.com>
2025-08-05 18:18:15 +0545

    fetched client using a fake rest api in client side

--- data_fetching/src/app/users-client/page.tsx
+++ data_fetching/src/app/users-client/page.tsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+"use client";
+import { useState, useEffect } from "react";
 
 type User = {
-  id: Number;
+  id: number;
   name: String;
   username: String;
   email: String;
@@ -10,7 +11,48 @@ type User = {
 
 export default function UserClient() {
   const [users, setUsers] = useState<User[]>();
-  const [laoding, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
-  return <div></div>;
+
+  useEffect(() => {
+    async function FetchUser() {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        if (!response.ok) throw new Error("failed to fetch user");
+        const data = await response.json();
+        setUsers(data);
+      } catch (err) {
+        if (err instanceof Error) {
+          setError(err.message);
+        } else {
+          setError("An Unknown Error Occured");
+        }
+      } finally {
+        setLoading(false);
+      }
+    }
+    FetchUser();
+  }, []);
+
+  if (loading) return <div>Loading</div>;
+  if (error) return <div>{error}</div>;
+  return (
+    <div>
+      <ul className="flex flex-col">
+        {users?.map((user) => (
+          <li
+            key={user.id}
+            className="p-4 bg-white shadow-md rounded-lg text-gray-700"
+          >
+            <div>{user.name}</div>
+            <div>username: {user.username}</div>
+            <div>email: {user.email}</div>
+            <div>phone: {user.phone}</div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
